8db81f5ad14469bd1b6e1a9ab4b1fd0b7af655dc
Jakub Motyka <j.motyka@stud.elka.pw.edu.pl>
2021-06-02 00:09:12 +0200

    statistics update

--- logger/statistics.py
+++ logger/statistics.py
@@ -2,17 +2,27 @@ import sys, os
 
 class Frame:
     def __init__(self, idx, clk, precam, poscam, pretxt, postxt, cons):
-        index = idx
-        clock = clk
-        preCamera = precam
-        postCamera = poscam
-        preASCII = pretxt
-        postASCII = postxt
-        console = cons
+        self.index = idx
+        self.clock = clk
+        self.preCamera = precam
+        self.postCamera = poscam
+        self.preASCII = pretxt
+        self.postASCII = postxt
+        self.console = cons
+    
+    def __str__(self):
+        return f'{self.index:5};{self.clock:.3f};{self.preCamera:.3f};{self.postCamera:.3f};{self.preASCII:.3f};{self.postASCII:.3f};{self.console:.3f}'
 
 class DiffFrame:
-    def __init__(frame):
-        
+    def __init__(self, frame):
+        self.clkToCam = frame.preCamera - frame.clock
+        self.cam = frame.postCamera - frame.preCamera
+        self.camToTxt = frame.preASCII - frame.postCamera
+        self.txt = frame.postASCII - frame.preASCII
+        self.txtToCon = frame.console - frame.postASCII
+    
+    def __str__(self):
+        return f'{self.clkToCam:.3f};{self.cam:.3f};{self.camToTxt:.3f};{self.txt:.3f};{self.txtToCon:.3f}'
 
 def parseFrames(log):
     frames = []
@@ -27,7 +37,8 @@ def parseFrames(log):
 def sumUpLog(log):
     frames = parseFrames(log)
     for f in frames:
-        print(f)
+        df = DiffFrame(f)
+        print(df)
 
 def main():
     logs = list(filter(lambda n: n.endswith('.txt'), os.listdir()))
